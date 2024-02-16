33a0fb3961bfc19dc98a35c0ad66f68ce271f92f
SunlyGH <95952079+SunlyGH@users.noreply.github.com>
2024-02-16 15:19:01 +0700

    Add files via upload

---
+++ CookBook/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CookBook.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string title = "";
+
+        private CuisineList cuisineList = new CuisineList();
+
+        [ObservableProperty]
+        private ObservableCollection<Cuisine> cuisines;
+
+        [ObservableProperty]
+        private Cuisine selectedCuisine;
+        public MainViewModel()
+        {
+            cuisines = new ObservableCollection<Cuisine>(cuisineList.Cuisines);
+            SelectedCuisine = cuisines[0];
+        }
+    }
+}
